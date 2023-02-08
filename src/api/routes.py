d03e296e5ac320539fe156efa75d3fc0f39e9dda
Andres Longueira <andreslongueirarengifo@gmail.com>
2023-02-08 19:33:32 +0000

    Add endpoint singup

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,9 +1,10 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-from flask import Flask, request, jsonify, url_for, Blueprint
+from flask import Flask, request, jsonify, url_for, Blueprint, json
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token,jwt_required, get_jwt_identity
 
 api = Blueprint('api', __name__)
 
@@ -16,3 +17,16 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/signup', methods=['POST'])
+def login_signup():
+    data = json.loads(request.data)
+    #user = User.query.filter_by(name=data["name"],lastname=data["lastname"],email=data["email"], password=data["password"]).first()
+    userObj = User(name=data["name"],lastname=data["lastname"],email=data["email"], password=data["password"])
+    #if user is None:
+        #return jsonify({"msg": "Bad username or password"}), 401
+    access_token = create_access_token(identity=user.id)
+    db.session.add(userObj)
+    db.session.commit()
+    #return jsonify({ "token": access_token, "user_id": user.id, "user": user }), 200
+    return jsonify({"ok":"ok"}), 200
