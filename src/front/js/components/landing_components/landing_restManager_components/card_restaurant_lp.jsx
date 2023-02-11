84f83e0bcd908f9478334be00ca0641aeacf29b7
Andres Longueira <andreslongueirarengifo@gmail.com>
2023-02-11 19:09:46 +0000

    Create view restManager

--- src/front/js/components/landing_components/landing_restManager_components/card_restaurant_lp.jsx
+++ src/front/js/components/landing_components/landing_restManager_components/card_restaurant_lp.jsx
@@ -6,10 +6,10 @@ import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 export const CardRestaurant =()=>{
     return (
-    <div className="card" style={{width: "18rem"}}>
+    <div className="card-rest d-flex flex-column" style={{width: "18rem", height: "25rem"}}>
         {/*<img src={logoWhite} class="card-img-top" alt="..."/>*/}
-        <h5 className="card-title m-auto"><b>Restaurante 1</b></h5>
-        <img src={logoWhite} className="img-card" ></img>
+        <h5 className="card-title p-2 m-auto mt-2"><b>Restaurante 1</b></h5>
+        <img src={logoWhite} className="img-card m-auto" ></img>
             <div className="card-body">
                 <h6><i><b>Url del restaurante</b></i></h6>
                     <CopyToClipboard text="restify.com/res/restaurant_1">
@@ -18,8 +18,10 @@ export const CardRestaurant =()=>{
                         <i className="fa-regular fa-copy"></i>
                     </div>
                     </CopyToClipboard>
-                <button className="btn-control-panel"><b>Panel de control</b> <i class="fa-solid fa-arrow-right"></i></button>
-                <button className="btn-web"><b>Panel de control</b> <i class="fa-solid fa-arrow-right"></i></button>
+                <div className="d-flex flex-column mt-3">
+                    <button className="btn-control-panel mb-2"><b>Panel de control</b> <i class="fa-solid fa-arrow-right"></i></button>
+                    <button className="btn-web"><b>Panel de control</b> <i class="fa-solid fa-arrow-right"></i></button>
+                </div>
             </div>
         </div>
     )
--- src/front/js/pages/landing/landing_restManager.jsx
+++ src/front/js/pages/landing/landing_restManager.jsx
@@ -6,14 +6,23 @@ import "../../../styles/landing.css";
 //import components
 import { NavbarRestManager } from "../../components/landing_components/landing_restManager_components/navbar_restManager_lp.jsx";
 import { CardRestaurant } from "../../components/landing_components/landing_restManager_components/card_restaurant_lp.jsx";
+import { CardListRest } from "../../components/landing_components/landing_restManager_components/card_list_restaurant_lp.jsx";
 
 export const RestManagerLP = () => {
   return (
     <div className="container-fluid p-0">
       <NavbarRestManager />
-      <h2>¡Bienvenido, Carlos!</h2>
-      <p>Gestión de restaurantes</p>
-      <CardRestaurant />
+      <div className="cont mt-3">
+        <h2 className="welcome">
+          <b>
+            ¡Bienvenido,<span>Carlos</span>!
+          </b>
+        </h2>
+        <p className="subtitle h5">
+          <b>Gestión de restaurantes</b>
+        </p>
+        <CardListRest />
+      </div>
     </div>
   );
 };
