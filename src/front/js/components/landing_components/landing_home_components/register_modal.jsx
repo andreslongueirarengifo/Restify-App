f2604314e22c27dec3cf89b08242833614da6c47
Andres Longueira <andreslongueirarengifo@gmail.com>
2023-02-21 17:07:08 +0000

    Add Fromik

--- src/front/js/components/landing_components/landing_home_components/register_modal.jsx
+++ src/front/js/components/landing_components/landing_home_components/register_modal.jsx
@@ -1,39 +1,52 @@
-import React, {useState} from "react";
-import {createUser} from "../../../service/user_service.js";
+import React, { useState } from "react";
+import { createUser } from "../../../service/user_service.js";
+
+import { Formik } from "formik";
 
 const RegisterModal = () => {
-
-  const [registerForm, setRegisterForm]= useState({})
+  const [registerForm, setRegisterForm] = useState({});
 
   const handleChange = (event) => {
-    setRegisterForm({...registerForm, [event.target.id]:event.target.value})
-  }
+    setRegisterForm({ ...registerForm, [event.target.id]: event.target.value });
+  };
 
-  const test = () =>{
-
-  }
+  const test = () => {};
 
   const handleClick = async (event) => {
     event.preventDefault();
-    const data = await createUser(registerForm)
-  }
-
+    const data = await createUser(registerForm);
+  };
 
   return (
     <>
       {/* <!-- Button trigger modal --> */}
-      <button type="button" className="btn btn-secondary btn-restify-primary btn-rounded" data-bs-toggle="modal" data-bs-target="#registerModal" style={{fontSize:"1.25rem"}}>
+      <button
+        type="button"
+        className="btn btn-secondary btn-restify-primary btn-rounded"
+        data-bs-toggle="modal"
+        data-bs-target="#registerModal"
+        style={{ fontSize: "1.25rem" }}
+      >
         Regístrate
       </button>
 
       {/* <!-- Modal --> */}
-      <div className="modal fade modal-fade-blur" id="registerModal" tabIndex="-1" aria-labelledby="registerModalLabel" aria-hidden="true">
+      <div
+        className="modal fade modal-fade-blur"
+        id="registerModal"
+        tabIndex="-1"
+        aria-labelledby="registerModalLabel"
+        aria-hidden="true"
+      >
         <div className="modal-dialog modal-lg modal-dialog-centered">
           <div className="modal-content modal-rounded-corners">
-            <h2 className="modal-title text-center" id="registerModalLabel">
+
+
+          <h2 className="modal-title text-center" id="registerModalLabel">
               Crea tu cuenta de Restify
             </h2>
             <div className="modal-body p-0 py-3">
+              
               <div className="row">
                 <div className="col">
                   <label htmlFor="name" className="form-label">Nombre</label>
@@ -64,6 +77,83 @@ const RegisterModal = () => {
             <button onClick={handleClick} type="button" className="btn btn-restify btn-restify-primary btn-form">
               Registrar usuario
             </button>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <Formik
+        initialValues={{ email: '', password: '' }}
+        validate={values => {
+          const errors = {};
+          if (!values.email) {
+            errors.email = 'Required';
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = 'Invalid email address';
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+         /* and other goodies */
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <input
+              type="email"
+              name="email"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.email}
+            />
+            {errors.email && touched.email && <div style={{color:"red"}}>{errors.email}</div>}
+
+
+
+
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </form>
+        )}
+      </Formik>
           </div>
         </div>
       </div>
