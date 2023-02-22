4c8bc46b4375f08295f96669acb31ce12ca663df
Carlos Alberto <95.carlos.velazquez@gmail.com>
2023-02-22 20:32:45 +0000

    dashboard to cp and cp views

--- src/front/js/components/controlpanel_components/navbar_cp.jsx
+++ src/front/js/components/controlpanel_components/navbar_cp.jsx
@@ -4,32 +4,32 @@ import { useNavigate } from "react-router-dom";
 //images
 import logoWhite from "../../../img/logo-white.png";
 
-export const NavbarCP = () => {
+export const NavbarCP = (props) => {
 
-    const navigate = useNavigate();
+	const navigate = useNavigate();
 
-    return (
-    <>
-    <ul class="nav flex-column vertical-navbar p-4">
-        <li class="nav-item">
-            <img src={logoWhite} alt="Restify" width="88" height="24" onClick={() => navigate(`/`)}/>
-        </li>
-        <li class="nav-item">
-        <a class="nav-link" aria-current="page" onClick={() => navigate(`/test-cp-home`)}>
-            Content
-        </a>
-        </li>
-        <li class="nav-item">
-        <a class="nav-link" onClick={() => navigate(`/test-cp-branding`)}>
-            Branding
-        </a>
-        </li>
-        <li class="nav-item">
-        <a class="nav-link" onClick={() => navigate(`/test-cp-menu`)}>
-            Menu
-        </a>
-        </li>
-    </ul>
-    </>
-);
+	return (
+		<div className="m-0">
+			<ul className="nav flex-column vertical-navbar p-4">
+				<li className="nav-item">
+					<img src={logoWhite} alt="Restify" width="88" height="24" onClick={() => navigate(`/`)} />
+				</li>
+				<li className="nav-item">
+					<a className="nav-link" aria-current="page" onClick={() => navigate(`/controlpanel/home/${props.webName}`)}>
+						Content
+					</a>
+				</li>
+				<li className="nav-item">
+					<a className="nav-link" onClick={() => navigate(`/controlpanel/branding/${props.webName}`)}>
+						Branding
+					</a>
+				</li>
+				<li className="nav-item">
+					<a className="nav-link" onClick={() => navigate(`/controlpanel/menu/${props.webName}`)}>
+						Menu
+					</a>
+				</li>
+			</ul>
+		</div>
+	);
 };
--- src/front/js/components/landing_components/landing_restManager_components/card_restaurant_lp.jsx
+++ src/front/js/components/landing_components/landing_restManager_components/card_restaurant_lp.jsx
@@ -1,65 +1,73 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-import logoWhite from "../../../../img/logo-white.png"
+import logoWhite from "../../../../img/logo-white.png";
 
-import {CopyToClipboard} from 'react-copy-to-clipboard';
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import { getRestaurant } from "../../../service/rest_manager_service";
 
-export const CardRestaurant =(props)=>{
+export const CardRestaurant = (props) => {
 
-    const [restaurantData, setRestaurantData] = useState([])
-    const [isLoading, setIsLoading] = useState(true)
-    
-    useEffect(() => {
-    const getRestaurantData = async () => {
-        const data = await getRestaurant(props.id);
-        setRestaurantData(data.result)
-        setIsLoading(false)
-    }
-    getRestaurantData();
-    },[])
+	const [restaurantData, setRestaurantData] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
 
-    if(isLoading){
-        return (
-        <div className="card-rest d-flex flex-column" style={{width: "18rem", height: "25rem"}}>
-            <p className="placeholder-glow mx-5 mt-2">
-                <span className="placeholder col-12 bg-secondary"></span>
-            </p>
-            <img src={logoWhite} className="img-card m-auto" ></img>
-                <div className="card-body">
-                    <label className="form-label">Url del restaurante</label>
-                        <div className="copy-text rounded justify-content-between align-items-center">
-                            <p className="placeholder-glow mx-5">
-                                <span className="placeholder col-12 bg-secondary"></span>
-                            </p>
-                            <i className="fa-regular fa-copy"></i>
-                        </div>
-                    <div className="d-flex flex-column mt-3">
-                        <button className="btn btn-restify-secondary btn-rounded mb-2">Panel de control</button>
-                        <button className="btn btn-restify-primary btn-rounded">Visualizar</button>
-                    </div>
-                </div>
-        </div>
-        )
-    }
+	useEffect(() => {
+		const getRestaurantData = async () => {
+			const data = await getRestaurant(props.id);
+			setRestaurantData(data.result);
+			setIsLoading(false);
+			console.log(data);
+		};
+		getRestaurantData();
+	}, []);
 
-    return (
-    <div className="card-rest d-flex flex-column" style={{width: "18rem", height: "25rem"}}>
-        <h5 className="card-title p-2 m-auto mt-2"><b>{restaurantData.name}</b></h5>
-        <img src={logoWhite} className="img-card m-auto" ></img>
-            <div className="card-body">
-                <label className="form-label">Url del restaurante</label>
-                    <CopyToClipboard text={"restify.com/res/"+restaurantData.name}>
-                    <div className="copy-text rounded justify-content-between align-items-center">
-                        <p className="copy-text-label m-0">{".../res/"+restaurantData.name}</p>
-                        <i className="fa-regular fa-copy"></i>
-                    </div>
-                    </CopyToClipboard>
-                <div className="d-flex flex-column mt-3">
-                    <button className="btn btn-restify-secondary btn-rounded mb-2">Panel de control</button>
-                    <button className="btn btn-restify-primary btn-rounded">Visualizar</button>
-                </div>
-            </div>
-        </div>
-    )
-}
+	if (isLoading) {
+		return (
+			<div className="card-rest d-flex flex-column" style={{ width: "18rem", height: "25rem" }}>
+				<p className="placeholder-glow mx-5 mt-2">
+					<span className="placeholder col-12 bg-secondary"></span>
+				</p>
+				<img src={logoWhite} className="img-card m-auto"></img>
+				<div className="card-body">
+					<label className="form-label">Url del restaurante</label>
+					<div className="copy-text rounded justify-content-between align-items-center">
+						<p className="placeholder-glow mx-5">
+							<span className="placeholder col-12 bg-secondary"></span>
+						</p>
+						<i className="fa-regular fa-copy"></i>
+					</div>
+					<div className="d-flex flex-column mt-3">
+						<button
+							className="btn btn-restify-secondary btn-rounded mb-2"
+						>
+							Panel de control
+						</button>
+						<button className="btn btn-restify-primary btn-rounded">Visualizar</button>
+					</div>
+				</div>
+			</div>
+		);
+	}
+
+	return (
+		<div className="card-rest d-flex flex-column" style={{ width: "18rem", height: "25rem" }}>
+			<h5 className="card-title p-2 m-auto mt-2">
+				<b>{restaurantData.name}</b>
+			</h5>
+			<img src={logoWhite} className="img-card m-auto"></img>
+			<div className="card-body">
+				<label className="form-label">Url del restaurante</label>
+				<CopyToClipboard text={"restify.com/res/" + restaurantData.name}>
+					<div className="copy-text rounded justify-content-between align-items-center">
+						<p className="copy-text-label m-0">{".../res/" + restaurantData.name}</p>
+						<i className="fa-regular fa-copy"></i>
+					</div>
+				</CopyToClipboard>
+				<div className="d-flex flex-column mt-3">
+                    <Link className="btn btn-restify-secondary btn-rounded mb-2" to={`/controlpanel/home/${props.name}`}>Panel de control</Link>
+					<button className="btn btn-restify-primary btn-rounded">Visualizar</button>
+				</div>
+			</div>
+		</div>
+	);
+};
