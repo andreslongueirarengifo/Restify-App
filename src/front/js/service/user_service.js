8bd43814a6f37808063eb01c6a9771b0292a80e5
Carlos Alberto <95.carlos.velazquez@gmail.com>
2023-04-05 16:04:42 +0000

    changed urls in services

--- src/front/js/service/user_service.js
+++ src/front/js/service/user_service.js
@@ -1,6 +1,6 @@
 export const createUser = async (formData) => {
-    try{
-        const response = await fetch(process.env.BACKEND_URL+"/api/signup", {
+    try {
+        const response = await fetch(process.env.BACKEND_URL + "/api/signup", {
             method: "POST",
             body: JSON.stringify(formData),
             headers: {
@@ -11,14 +11,14 @@ export const createUser = async (formData) => {
         localStorage.setItem("jwt-token", data.token);
         return data;
     }
-    catch(e){
+    catch (e) {
         return e
     }
 }
 
 export const loginUser = async (formData) => { // formData = {email: "sample@email.com", password: "123456"}
-    try{
-        const response = await fetch(process.env.BACKEND_URL+"/api/login", {
+    try {
+        const response = await fetch(process.env.BACKEND_URL + "/api/login", {
             method: "POST",
             body: JSON.stringify(formData),
             headers: {
@@ -29,39 +29,39 @@ export const loginUser = async (formData) => { // formData = {email: "sample@ema
         localStorage.setItem("jwt-token", data.token);
         return data;
     }
-    catch(e){
+    catch (e) {
         return e
     }
 }
 
 export const getCurrentUser = async () => {
-	try {
-		const token = localStorage.getItem("jwt-token");
-		const response = await fetch("https://sample-service-name-tv81.onrender.com" + "/api/currentuser", {
-			method: "GET",
-			headers: {
-				"Content-Type": "application/json",
-				Authorization: "Bearer " + token, // ⬅⬅⬅ authorization token
-			},
-		});
-		const data = await response.json();
-		return data;
-	} catch (e) {
-		return e;
-	}
+    try {
+        const token = localStorage.getItem("jwt-token");
+        const response = await fetch(process.env.BACKEND_URL + "/api/currentuser", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: "Bearer " + token, // ⬅⬅⬅ authorization token
+            },
+        });
+        const data = await response.json();
+        return data;
+    } catch (e) {
+        return e;
+    }
 };
 
 export const getUsers = async () => {
-	try {
-		const response = await fetch("https://sample-service-name-tv81.onrender.com" + "/api/users", {
-			method: "GET",
-			headers: {
-				"Content-Type": "application/json",
-			},
-		});
-		const data = await response.json();
-		return data;
-	} catch (e) {
-		return e;
-	}
+    try {
+        const response = await fetch(process.env.BACKEND_URL + "/api/users", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        const data = await response.json();
+        return data;
+    } catch (e) {
+        return e;
+    }
 };
